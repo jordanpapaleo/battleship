1eaf2511befe207d7ae3315caf5c0f1c6148199a
Jordan Papaleo <papaleowebdev@gmail.com>
2020-09-14 16:34:42 -0700

    Added ship placement without collision detection.  Also added hit or miss .

--- src/index.js
+++ src/index.js
@@ -1,39 +1,85 @@
 const rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
 
-// const board = rows.reduce((map, next) => {
-//   map.set(next, new Array(rows.length).fill(''))
-//   return map
-// }, new Map())
-const board = rows.reduce((map, next) => {
-  // map.set(next, )
-  return {
-    ...map,
-    [next]: new Array(rows.length - 1).fill(' ')
+function initShip(name, size) {
+  return { name, size, x: null, y: null }
+}
+
+const ships = {
+  battleship: initShip('battleship', 4),
+  carrier: initShip('carrier', 5),
+  cruiser: initShip('cruiser', 3),
+  destroyer: initShip('destroyer', 2),
+  submarine: initShip('submarine', 3),
+}
+
+const board = rows.reduce((map, next) => ({ ...map, [next]: new Array(rows.length - 1).fill(' ') }), {})
+
+function placeShip(type, row, column, alignment) {
+  ships[type].x = column
+  ships[type].y = row
+
+  let size = ships[type].size
+
+  while(size) {
+    if (alignment === 'H') {
+        const c = column - 1 + size - 1
+        if (c < 9 && c >= 0) {
+          board[row][c] = 'X'
+        } else {
+          console.log('Column off board')
+        }
+
+    } else if (alignment === 'V') {
+      const rowI = rows.indexOf(row)
+
+      if (rowI + size === -1) {
+        console.log('Not valid row')
+      } else {
+        board[rows[rowI + size - 1]][column - 1] = 'X'
+      }
+    }
+
+    size--
   }
-}, {})
+}
 
 
 function fire(row, column) {
-  board[row][column - 1] = 'X'
+  if (board[row][column - 1] === 'X') {
+    board[row][column - 1] = 'H'
+    console.log('\x1b[31m%s\x1b[0m', `Fire ${row} x ${column}: HIT`)
+  } else {
+    board[row][column - 1] = 'M'
+    console.log('\x1b[37m%s\x1b[0m', `Fire ${row} x ${column}: MISS`)
+  }
 }
 
-function logBoard(board) {
+function logRow(row) {
+  return row.reduce((current, next, i) => `${current}[${row[i]}] `, ``)
+}
+
+function logBoard(b) {
   console.log(`
      1   2   3   4   5   6   7   8   9
   ------------------------------------
-A | ${board.A.reduce((current, next, i) => `${current}[${board.A[i]}] `, ``)}
-B | ${board.B.reduce((current, next, i) => `${current}[${board.B[i]}] `, ``)}
-C | ${board.C.reduce((current, next, i) => `${current}[${board.C[i]}] `, ``)}
-D | ${board.D.reduce((current, next, i) => `${current}[${board.D[i]}] `, ``)}
-E | ${board.E.reduce((current, next, i) => `${current}[${board.E[i]}] `, ``)}
-F | ${board.F.reduce((current, next, i) => `${current}[${board.F[i]}] `, ``)}
-G | ${board.G.reduce((current, next, i) => `${current}[${board.G[i]}] `, ``)}
-H | ${board.H.reduce((current, next, i) => `${current}[${board.H[i]}] `, ``)}
-I | ${board.I.reduce((current, next, i) => `${current}[${board.I[i]}] `, ``)}
-J | ${board.J.reduce((current, next, i) => `${current}[${board.J[i]}] `, ``)}
+A | ${logRow(b.A)}
+B | ${logRow(b.B)}
+C | ${logRow(b.C)}
+D | ${logRow(b.D)}
+E | ${logRow(b.E)}
+F | ${logRow(b.F)}
+G | ${logRow(b.G)}
+H | ${logRow(b.H)}
+I | ${logRow(b.I)}
+J | ${logRow(b.J)}
 `)
-
 }
+
+placeShip('carrier', 'B', 2, 'V')
+placeShip('battleship', 'C', 5, 'H')
+placeShip('cruiser', 'F', 4, 'V')
+placeShip('submarine', 'H', 6, 'V')
+placeShip('destroyer', 'F', 7, 'H')
 
 fire('A', 1)
 fire('B', 2)
@@ -41,5 +87,6 @@ fire('C', 3)
 fire('C', 2)
 fire('C', 1)
 fire('J', 9)
+fire('C', 6)
 
 logBoard(board)
